0bb802ea2b8ffe49269230c471d5ff269c201bb0
Dayse Santos <daysedesign@gmail.com>
2025-05-05 14:19:44 -0300

    Trabalhando com o fila rabbitmq

---
+++ src/main/java/com/in28minutes/microservices/camel_microservice_a/routes/c/ActiveMqSenderRouter.java
@@ -0,0 +1,20 @@
+package com.in28minutes.microservices.camel_microservice_a.routes.c;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ActiveMqSenderRouter extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        //timer
+        from("timer:active-mq-timer?period=10000")
+        .log("${body}")
+        .transform().constant("My messege for Active MQ")
+        .log("${body}")
+        .to("activemq:my-activemq-queue");
+        //queue
+    }
+    
+}
