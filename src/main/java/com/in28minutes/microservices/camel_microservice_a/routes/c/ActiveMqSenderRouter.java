2974a52b4ff0ea03e10d19096b774726cf520733
Dayse Santos <daysedesign@gmail.com>
2025-05-05 16:44:39 -0300

    enviando arquivo json

--- src/main/java/com/in28minutes/microservices/camel_microservice_a/routes/c/ActiveMqSenderRouter.java
+++ src/main/java/com/in28minutes/microservices/camel_microservice_a/routes/c/ActiveMqSenderRouter.java
@@ -9,12 +9,16 @@ public class ActiveMqSenderRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         //timer
-        from("timer:active-mq-timer?period=10000")
-        .log("${body}")
-        .transform().constant("My messege for Active MQ")
+        // from("timer:active-mq-timer?period=10000")
+        // .log("${body}")
+        // .transform().constant("My messege for Active MQ")
+        // .log("${body}")
+        // .to("activemq:my-activemq-queue");
+        //queue
+
+        from("file:files/json")
         .log("${body}")
         .to("activemq:my-activemq-queue");
-        //queue
     }
     
 }
