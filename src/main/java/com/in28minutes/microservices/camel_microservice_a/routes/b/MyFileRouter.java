7b8e1015dab879c8a06487cf200c559500aa2891
Dayse Santos <daysedesign@gmail.com>
2025-05-09 21:53:46 -0300

    Aprendendo trabalar com .choice()

--- src/main/java/com/in28minutes/microservices/camel_microservice_a/routes/b/MyFileRouter.java
+++ src/main/java/com/in28minutes/microservices/camel_microservice_a/routes/b/MyFileRouter.java
@@ -8,7 +8,18 @@ public class MyFileRouter extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
+        // from("file:files/input")
+        // .log("${body}")
+        // .to("file:files/output");
+
         from("file:files/input")
+        .routeId("Files-Input-Route")
+        .choice()
+            .when(simple("${file:ext} ends with 'xml'"))
+                .log("XML FILE")
+            .otherwise()
+                .log("Not an XML FILE")
+        .end()
         .log("${body}")
         .to("file:files/output");
     }
